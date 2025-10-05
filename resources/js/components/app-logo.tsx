715b89ea724159c03c89577a8f6b6843e5cf2c9b
evanightly <adamhenderson3x3@gmail.com>
2025-10-06 04:05:05 +0700

    update: app logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,15 +1,16 @@
-import AppLogoIcon from './app-logo-icon';
+import { usePage } from '@inertiajs/react';
+import { Leaf } from 'lucide-react';
 
 export default function AppLogo() {
+    const page = usePage();
+
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className='flex aspect-square size-8 items-center justify-center rounded-md'>
+                <Leaf className='h-8 w-8 text-emerald-600 dark:text-emerald-400' />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
-                </span>
+            <div className='ml-1 grid flex-1 text-left text-sm'>
+                <span className='mb-0.5 truncate leading-tight font-semibold'>{page.props.name as unknown as string}</span>
             </div>
         </>
     );
